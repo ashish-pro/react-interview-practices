e264ef5c8fda62563b85847bfa986ffffbfd3706
ashish-pro <ashishbit017@outlook.com>
2024-05-07 16:52:19 +0530

    accordian - single and multiple

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Accordian from './components/accordian/Accordian';
 
 function App() {
   return (
     <div className="App">
-      
+      <Accordian/>
     </div>
   );
 }
